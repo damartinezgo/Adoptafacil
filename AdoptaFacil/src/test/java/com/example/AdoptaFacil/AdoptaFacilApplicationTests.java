b55f00ffa7a5c3eb89668597debc1a483655d746
BrayanSanchez <brayan.esteban438@gmail.com>
2025-09-13 08:00:48 -0500

    Estructura del proyecto

---
+++ AdoptaFacil/src/test/java/com/example/AdoptaFacil/AdoptaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdoptaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
